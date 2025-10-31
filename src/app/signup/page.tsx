59dca23eec75deeac20bf718153a42e298e98c11
imranprof <imranpers@gmail.com>
2025-11-01 03:41:09 +0600

    feat: add signup page and update login page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,4 @@
-// app/login/page.tsx
+
 'use client';
 
 import { useState } from 'react';
@@ -10,12 +10,13 @@ import Link from 'next/link';
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const setToken = useAuthStore((state) => state.setToken);
   const router = useRouter();
 
-  const handleLogin = async () => {
+  const handleSignup = async () => {
     try {
-      const res = await axios.post('/auth/login', { email, password });
+      const res = await axios.post('/auth/signup', { email, username, password });
       setToken(res.data.access_token);
       router.push('/dashboard');
     } catch (err) {
@@ -27,7 +28,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h1>
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">SignUp</h1>
 
         <div className="space-y-4">
           <input
@@ -35,6 +36,12 @@ export default function LoginPage() {
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
@@ -47,13 +54,13 @@ export default function LoginPage() {
 
         <button
           className="w-full mt-6 bg-blue-600 text-white font-semibold py-3 rounded-lg shadow-md hover:bg-blue-700 transition"
-          onClick={handleLogin}
+          onClick={handleSignup}
         >
           Login
         </button>
 
         <p className="mt-4 text-center text-gray-500 text-sm">
-          Don't have an account? <Link href={"/signup"} className="text-blue-600 hover:underline cursor-pointer">Signup</Link>
+          Already Have Account? <Link href={"/login"} className="text-blue-600 hover:underline cursor-pointer">Login</Link>
         </p>
       </div>
     </div>
