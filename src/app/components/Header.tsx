a637782d274938caae3917afebaac3de061c47a7
imranprof <imranpers@gmail.com>
2025-11-01 04:35:01 +0600

    modify header for dehydration error

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import Link from 'next/link';
-import { useAuthStore } from "../../store/useStore"
+import { useAuthStore } from "../../store/useStore";
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const { token, logout } = useAuthStore();
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleLogout = () => {
     logout();
@@ -20,31 +26,33 @@ export default function Header() {
         <Link href="/dashboard">Task Manager</Link>
       </h1>
 
-      <nav className="flex gap-4">
-        {!token ? (
-          <>
-            <Link
-              href="/login"
-              className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded transition"
+      {mounted && (
+        <nav className="flex gap-4">
+          {!token ? (
+            <>
+              <Link
+                href="/login"
+                className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded transition"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded transition"
+              >
+                Signup
+              </Link>
+            </>
+          ) : (
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded transition"
             >
-              Login
-            </Link>
-            <Link
-              href="/signup"
-              className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded transition"
-            >
-              Signup
-            </Link>
-          </>
-        ) : (
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded transition"
-          >
-            Logout
-          </button>
-        )}
-      </nav>
+              Logout
+            </button>
+          )}
+        </nav>
+      )}
     </header>
   );
 }
